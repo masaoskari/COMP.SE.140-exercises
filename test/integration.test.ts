822e644875fd34a97f87f44042257015048fc395
masaoskari <masaoskari.linna@gmail.com>
2024-12-20 23:31:01 +0200

    try to fix tests

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -11,7 +11,18 @@ describe("API Gateway Integration Tests", () => {
   it("should return service information from /api", async () => {
     const response = await request(baseUrl).get("/api").auth("nginx", "nginx");
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("data");
+    expect(response.body).toHaveProperty("service1");
+    expect(response.body).toHaveProperty("service2");
+    expect(response.body.service1).toHaveProperty("diskSpace");
+    expect(response.body.service1).toHaveProperty("ipAddresses");
+    expect(response.body.service1).toHaveProperty("osUptime");
+    expect(response.body.service1).toHaveProperty("processes");
+    expect(response.body.service1).toHaveProperty("serviceUptime");
+    expect(response.body.service2).toHaveProperty("diskSpace");
+    expect(response.body.service2).toHaveProperty("ipAddresses");
+    expect(response.body.service2).toHaveProperty("osUptime");
+    expect(response.body.service2).toHaveProperty("processes");
+    expect(response.body.service2).toHaveProperty("serviceUptime");
   });
 
   it("should stop the service from /api/stop", async () => {
@@ -22,5 +33,5 @@ describe("API Gateway Integration Tests", () => {
     expect(response.text).toBe(
       "Stopping containers. See from the terminal more information."
     );
-  });
+  }, 10000);
 });

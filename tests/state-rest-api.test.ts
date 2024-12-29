2ec9ff4ddbb7eaec7320cd766baf5a99fac7aae6
ckmali <matti.linna@tuni.fi>
2024-12-29 21:58:39 +0200

    add get run log test

--- tests/state-rest-api.test.ts
+++ tests/state-rest-api.test.ts
@@ -3,11 +3,49 @@ import request from "supertest";
 describe("Rest API Tests", () => {
   const baseUrl = "http://localhost:8197";
 
+  beforeEach(() => {
+    // Reset the state of the system before each test
+    return request(baseUrl)
+      .put("/state")
+      .auth("nginx", "nginx")
+      .send("INIT")
+      .set("Content-Type", "text/plain");
+  });
+
   it("Should get the state of the system", async () => {
     const response = await request(baseUrl).get("/state");
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toMatch(/text\/plain/);
     expect(response.text).toBe("INIT");
+  });
+
+  it("Should get the run log", async () => {
+    // Do some state transitions
+    await request(baseUrl)
+      .put("/state")
+      .auth("nginx", "nginx")
+      .send("RUNNING")
+      .set("Content-Type", "text/plain");
+
+    await request(baseUrl)
+      .put("/state")
+      .auth("nginx", "nginx")
+      .send("PAUSED")
+      .set("Content-Type", "text/plain");
+
+    await request(baseUrl)
+      .put("/state")
+      .auth("nginx", "nginx")
+      .send("RUNNING")
+      .set("Content-Type", "text/plain");
+
+    // Get the run log and check the state transitions
+    const response = await request(baseUrl).get("/run-log");
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toMatch(/text\/plain/);
+    expect(response.text).toMatch(/INIT -> RUNNING/);
+    expect(response.text).toMatch(/RUNNING -> PAUSED/);
+    expect(response.text).toMatch(/PAUSED -> RUNNING/);
   });
 
   it("Should change the state of the system", async () => {

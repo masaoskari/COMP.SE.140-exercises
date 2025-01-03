1b6a636980bdc67dcded6df6592b0d5ff59750a2
ckmali <matti.linna@tuni.fi>
2025-01-03 09:05:51 +0200

    adds monitoring service tests

--- tests/browser-endpoint.test.ts
+++ tests/browser-endpoint.test.ts
@@ -3,30 +3,57 @@ import { exec } from "child_process";
 import util from "util";
 
 const execPromise = util.promisify(exec);
+const monitorUrl = "http://localhost:8098";
+const restApiUrl = "http://localhost:8197";
+const browserAppUrl = "http://localhost:8198";
+
+describe("Monitoring service tests", () => {
+  it("Should serve the monitoring HTML page", async () => {
+    const response = await request(monitorUrl).get("/");
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toMatch(/text\/html/);
+  });
+
+  it("should return monitoring data", async () => {
+    const response = await request(monitorUrl).get("/info");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("startTime");
+    expect(response.body).toHaveProperty("requestCount");
+    expect(response.body.requestCount).toBe(0);
+
+    // Send one request to rest api and browser app and
+    // check if the request count is updated
+    await request(restApiUrl).get("/state");
+    await request(browserAppUrl).get("/");
+
+    const updatedResponse = await request(monitorUrl).get("/info");
+    expect(updatedResponse.status).toBe(200);
+    expect(updatedResponse.body).toHaveProperty("requestCount");
+    expect(updatedResponse.body.requestCount).toBe(2);
+  });
+});
 
 describe("Browser Integration Tests", () => {
-  const baseUrl = "http://localhost:8198";
-  const restApiUrl = "http://localhost:8197";
 
   it("should return 401 for unauthorized access", async () => {
-    const response = await request(baseUrl).get("/");
+    const response = await request(browserAppUrl).get("/");
     expect(response.status).toBe(401);
   });
 
   it("Should not be able to fetch data from api if app is not running state", async () => {
-    const response = await request(baseUrl).get("/request").auth("nginx", "nginx");
+    const response = await request(browserAppUrl).get("/request").auth("nginx", "nginx");
     expect(response.status).toBe(503);
     expect(response.text).toBe("Service is not in running state.");
   });
 
   it("Should not be able to stop the service if app is not running state", async () => {
-    const response = await request(baseUrl).post("/stop").auth("nginx", "nginx");
+    const response = await request(browserAppUrl).post("/stop").auth("nginx", "nginx");
     expect(response.status).toBe(503);
     expect(response.text).toBe("Service can be stopped only when it is in running or paused state.");
   });
 
   it("Should set app state to RUNNING when successfully login", async () => {
-    const response = await request(baseUrl).get("/").auth("nginx", "nginx");
+    const response = await request(browserAppUrl).get("/").auth("nginx", "nginx");
     expect(response.status).toBe(200);
 
     const stateResponse = await request(restApiUrl).get("/state");
@@ -35,7 +62,7 @@ describe("Browser Integration Tests", () => {
   });
 
   it("should return service information from /api", async () => {
-    const response = await request(baseUrl).get("/request").auth("nginx", "nginx");
+    const response = await request(browserAppUrl).get("/request").auth("nginx", "nginx");
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("service1");
     expect(response.body).toHaveProperty("service2");
@@ -67,7 +94,7 @@ describe("Browser Integration Tests", () => {
   });
 
   it("should stop the service from /api/stop", async () => {
-    const response = await request(baseUrl)
+    const response = await request(browserAppUrl)
       .post("/stop")
       .auth("nginx", "nginx");
     expect(response.status).toBe(200);
